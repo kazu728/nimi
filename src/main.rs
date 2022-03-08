17d4921f88f2cf06e11b87aee3602cfc37d77059
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-03-08 15:53:23 -0800

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use core::panic;
 use std::env;
+use std::process::exit;
 use std::str::Chars;
 
+#[derive(Debug)]
+enum ParseError {
+    Eof,
+    UnexpectedError,
+}
+
 // e.g. "0x20a" -> 'a'
-fn extract_next_char(chars: &mut Chars) -> char {
+fn extract_next_char(chars: &mut Chars) -> Result<char, ParseError> {
     while let Some(c) = chars.next() {
         if !c.is_whitespace() {
-            return c;
+            return Ok(c);
         }
     }
-
-    panic!("No more chars left");
+    Err(ParseError::Eof)
 }
 
 // e.g. ['1', '0', '0'] -> 100
@@ -29,25 +36,25 @@ fn convert_to_numeric(c: char, chars: &mut Chars) -> i32 {
     val
 }
 
-fn eval_string(buffer: String, vec: &mut Vec<String>) -> i32 {
+fn eval_string(buffer: String, vec: &mut Vec<String>) -> Result<i32, ParseError> {
     let chars = &mut buffer.chars();
-    let number = convert_to_numeric(chars.next().unwrap(), chars);
+    let number = convert_to_numeric(extract_next_char(chars)?, chars);
 
     let expression = vec.get(0).unwrap().clone();
 
     eval(&mut expression.chars(), number, vec)
 }
 
-fn fn_apply(chars: &mut Chars, vec: &mut Vec<String>) -> i32 {
+fn fn_apply(chars: &mut Chars, vec: &mut Vec<String>) -> Result<i32, ParseError> {
     let mut buffer = "".to_string();
 
     loop {
-        let nextchar = chars.next().unwrap();
+        let nextchar = extract_next_char(chars)?;
         match nextchar {
             '(' => (),
             ')' => break,
-            'f' if chars.next().unwrap() == 'n' => {
-                let ret = fn_apply(chars, vec);
+            'f' if extract_next_char(chars)? == 'n' => {
+                let ret = fn_apply(chars, vec)?;
                 buffer.push_str(&ret.to_string());
             }
             _ => buffer.push(nextchar),
@@ -57,42 +64,47 @@ fn fn_apply(chars: &mut Chars, vec: &mut Vec<String>) -> i32 {
     eval_string(buffer, vec)
 }
 
-fn fn_define(chars: &mut Chars, vec: &mut Vec<String>) {
+fn fn_define(chars: &mut Chars, vec: &mut Vec<String>) -> Result<(), ParseError> {
     let mut buffer = "".to_string();
 
     loop {
-        let c = chars.next().unwrap();
+        let c = match chars.next() {
+            Some(c) => Ok(c),
+            None => Err(ParseError::Eof),
+        }?;
+
         match c {
             ']' => break,
             _ => buffer.push(c),
         }
     }
     vec.push(buffer);
+    Ok(())
 }
 
-fn eval(chars: &mut Chars, arg: i32, vec: &mut Vec<String>) -> i32 {
-    let c = extract_next_char(chars);
+fn eval(chars: &mut Chars, arg: i32, vec: &mut Vec<String>) -> Result<i32, ParseError> {
+    let c = extract_next_char(chars)?;
 
     if c == '.' {
         chars.next();
-        return arg;
+        return Ok(arg);
     }
 
-    if c == 'f' && chars.next().unwrap() == 'n' {
-        match chars.next().unwrap() {
-            '[' => fn_define(chars, vec),
+    if c == 'f' && extract_next_char(chars)? == 'n' {
+        match extract_next_char(chars)? {
+            '[' => fn_define(chars, vec)?,
             '(' => return fn_apply(chars, vec),
-            _ => unimplemented!(),
+            _ => return Err(ParseError::UnexpectedError),
         }
     }
     if c.is_digit(10) {
-        return convert_to_numeric(c, chars);
+        return Ok(convert_to_numeric(c, chars));
     }
 
     if c == '+' || c == '-' || c == '*' || c == '/' {
-        let x = eval(chars, arg, vec);
-        let y = eval(chars, arg, vec);
-        return match c {
+        let x = eval(chars, arg, vec)?;
+        let y = eval(chars, arg, vec)?;
+        return Ok(match c {
             '+' => x + y,
             '-' => x - y,
             '*' => x * y,
@@ -101,7 +113,7 @@ fn eval(chars: &mut Chars, arg: i32, vec: &mut Vec<String>) -> i32 {
                 vec.pop();
                 unimplemented!()
             }
-        };
+        });
     };
 
     eval(chars, arg, vec)
@@ -111,7 +123,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut vec: Vec<String> = vec![];
 
-    eval(&mut args[1].chars(), 0, &mut vec);
+    match eval(&mut args[1].chars(), 0, &mut vec) {
+        Ok(_) => exit(0),
+        Err(ParseError::Eof) => exit(0),
+        Err(ParseError::UnexpectedError) => panic!(),
+    }
 }
 
 #[cfg(test)]
@@ -124,31 +140,58 @@ mod tests {
 
     #[test]
     fn arithmetic_operation() {
-        assert_eq!(eval(&mut "+ 30 20".chars(), 0, &mut gen_vec()), 50);
-        assert_eq!(eval(&mut "+ 40 + 10 20".chars(), 0, &mut gen_vec()), 70);
-        assert_eq!(eval(&mut "+ + 100 10 20".chars(), 0, &mut gen_vec()), 130);
-        assert_eq!(eval(&mut "+ 200 1".chars(), 0, &mut gen_vec()), 201);
-        assert_eq!(eval(&mut "- 30 20".chars(), 0, &mut gen_vec()), 10);
-        assert_eq!(eval(&mut "- 20 30".chars(), 0, &mut gen_vec()), -10);
-        assert_eq!(eval(&mut "* 10 10".chars(), 0, &mut gen_vec()), 100);
-        assert_eq!(eval(&mut "/ 100 5".chars(), 0, &mut gen_vec()), 20);
+        assert_eq!(eval(&mut "+ 30 20".chars(), 0, &mut gen_vec()).unwrap(), 50);
+        assert_eq!(
+            eval(&mut "+ 40 + 10 20".chars(), 0, &mut gen_vec()).unwrap(),
+            70
+        );
+        assert_eq!(
+            eval(&mut "+ + 100 10 20".chars(), 0, &mut gen_vec()).unwrap(),
+            130
+        );
+        assert_eq!(
+            eval(&mut "+ 200 1".chars(), 0, &mut gen_vec()).unwrap(),
+            201
+        );
+        assert_eq!(eval(&mut "- 30 20".chars(), 0, &mut gen_vec()).unwrap(), 10);
+        assert_eq!(
+            eval(&mut "- 20 30".chars(), 0, &mut gen_vec()).unwrap(),
+            -10
+        );
+        assert_eq!(
+            eval(&mut "* 10 10".chars(), 0, &mut gen_vec()).unwrap(),
+            100
+        );
+        assert_eq!(eval(&mut "/ 100 5".chars(), 0, &mut gen_vec()).unwrap(), 20);
     }
     #[test]
     fn function() {
-        assert_eq!(eval(&mut "fn[+ . .] fn(1)".chars(), 0, &mut gen_vec()), 2);
-        assert_eq!(eval(&mut "fn[+ . .] fn(10)".chars(), 0, &mut gen_vec()), 20);
-        assert_eq!(eval(&mut "fn[+ . 2] fn(5)".chars(), 0, &mut gen_vec()), 7);
-        assert_eq!(eval(&mut "fn[* . 2] fn(5)".chars(), 0, &mut gen_vec()), 10);
         assert_eq!(
-            eval(&mut "fn[* . .] fn(10)".chars(), 0, &mut gen_vec()),
+            eval(&mut "fn[+ . .] fn(1)".chars(), 0, &mut gen_vec()).unwrap(),
+            2
+        );
+        assert_eq!(
+            eval(&mut "fn[+ . .] fn(10)".chars(), 0, &mut gen_vec()).unwrap(),
+            20
+        );
+        assert_eq!(
+            eval(&mut "fn[+ . 2] fn(5)".chars(), 0, &mut gen_vec()).unwrap(),
+            7
+        );
+        assert_eq!(
+            eval(&mut "fn[* . 2] fn(5)".chars(), 0, &mut gen_vec()).unwrap(),
+            10
+        );
+        assert_eq!(
+            eval(&mut "fn[* . .] fn(10)".chars(), 0, &mut gen_vec()).unwrap(),
             100
         );
         assert_eq!(
-            eval(&mut "fn[* . .] fn(fn(2))".chars(), 0, &mut gen_vec()),
+            eval(&mut "fn[* . .] fn(fn(2))".chars(), 0, &mut gen_vec()).unwrap(),
             16
         );
         assert_eq!(
-            eval(&mut "fn[* . .] fn(fn(fn(2)))".chars(), 0, &mut gen_vec()),
+            eval(&mut "fn[* . .] fn(fn(fn(2)))".chars(), 0, &mut gen_vec()).unwrap(),
             256
         );
         assert_eq!(
@@ -156,7 +199,8 @@ mod tests {
                 &mut "fn[* . .] fn(fn(fn(fn(2))))".chars(),
                 0,
                 &mut gen_vec()
-            ),
+            )
+            .unwrap(),
             65536
         );
     }
